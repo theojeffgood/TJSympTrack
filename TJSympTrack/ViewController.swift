3cc64c6dc7800db31035a0810a38c1e487470de2
Theo <theo.goodman@gmail.com>
2020-04-15 09:56:19 -0400

    Initial Commit

---
+++ TJSympTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TJSympTrack
+//
+//  Created by Theo Goodman on 4/15/20.
+//  Copyright © 2020 Theo Goodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
